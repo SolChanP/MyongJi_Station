1331054899007450ed513f019336e2d4fc7c770d
Parksolchan <qkrthfcks1@naver.com>
2020-11-11 22:48:30 +0900

    실시간 게시판 PHP

---
+++ 실시간 게시판PHP/getLive1.php
@@ -0,0 +1,34 @@
+<?php 
+
+    error_reporting(E_ALL); 
+    ini_set('display_errors',1); 
+
+    include('dbcon.php');
+        
+
+    $stmt = $con->prepare('select * from board where line = 1');
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0)
+    {
+        $sub = array(); 
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            extract($row);
+    
+            array_push($sub, 
+                array('line'=>$line,
+                'train'=>$train,
+                'data'=>$data,
+	    'num'=>$num,
+	    'title'=>$title
+            ));
+        }
+
+        header('Content-Type: application/json; charset=utf8');
+        $json = json_encode(array("실시간데이터"=>$sub), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+        echo $json;
+    }
+
+?>
